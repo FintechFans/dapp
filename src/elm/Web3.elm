a481d87df3d514eb869f0eda6286d06157f49940
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-06-29 17:22:19 +0200

    Split off Hex handling into Utils

--- src/elm/Web3.elm
+++ src/elm/Web3.elm
@@ -5,8 +5,12 @@ import Json.Decode as Decode
 import Porter
 import Msgs exposing (Msg)
 import Web3.Types exposing (Web3RPCCall, Web3RPCResponse(..), Request, Config, Error)
+import Web3.Utils
+
 
 port outgoing : Encode.Value -> Cmd msg
+
+
 port incoming : (Decode.Value -> msg) -> Sub msg
 
 
@@ -23,10 +27,11 @@ porterConfig =
 web3_call_encode : Web3RPCCall -> Encode.Value
 web3_call_encode web3_rpc_call =
     Encode.object
-        [ ("jsonrpc", Encode.string "2.0")
-        , ("method", Encode.string web3_rpc_call.method)
-        , ("params", web3_rpc_call.params |> List.map Encode.string |> Encode.list)
+        [ ( "jsonrpc", Encode.string "2.0" )
+        , ( "method", Encode.string web3_rpc_call.method )
+        , ( "params", web3_rpc_call.params |> List.map Encode.string |> Encode.list )
         ]
+
 
 web3_call_decode : Decode.Decoder Web3RPCResponse
 web3_call_decode =
@@ -34,15 +39,17 @@ web3_call_decode =
         successful_response_decoder =
             Decode.field "result" Decode.value
                 |> Decode.map SuccessfulResponse
+
         error_response_decoder =
             Decode.field "error" <|
-            Decode.map2 ErrorResponse
-                (Decode.field "code" Decode.int)
-                (Decode.field "message" Decode.string)
+                Decode.map2 ErrorResponse
+                    (Decode.field "code" Decode.int)
+                    (Decode.field "message" Decode.string)
     in
-        Decode.oneOf [ successful_response_decoder
-                     , error_response_decoder
-                     ]
+        Decode.oneOf
+            [ successful_response_decoder
+            , error_response_decoder
+            ]
 
 
 subscriptions : Sub Msg
@@ -51,22 +58,25 @@ subscriptions =
 
 
 update porter_msg model =
-            let
-                (porter_model, porter_cmd) =
-                    Porter.update porterConfig porter_msg model.web3_porter
-            in
-                -- Debug.log (toString porter_msg)
-                    ({ model | web3_porter = porter_model }, porter_cmd)
+    let
+        ( porter_model, porter_cmd ) =
+            Porter.update porterConfig porter_msg model.web3_porter
+    in
+        -- Debug.log (toString porter_msg)
+        ( { model | web3_porter = porter_model }, porter_cmd )
 
-request : Web3RPCCall -> (Web3RPCResponse -> (Result Error res)) -> Request res
+
+request : Web3RPCCall -> (Web3RPCResponse -> Result Error res) -> Request res
 request call_info request_handler =
     Porter.fancyRequest call_info request_handler
+
 
 send : Config msg -> (Result Error res -> msg) -> Request res -> Cmd msg
 send config msg_handler req =
     Porter.send config msg_handler req
 
-decodeJSONRPCResult : (Decode.Decoder res) -> Web3RPCResponse -> Result Error res
+
+decodeJSONRPCResult : Decode.Decoder res -> Web3RPCResponse -> Result Error res
 decodeJSONRPCResult decoder response =
     case response of
         SuccessfulResponse val ->
@@ -75,28 +85,50 @@ decodeJSONRPCResult decoder response =
                 |> Decode.decodeValue decoder
                 |> Result.mapError Web3.Types.ResultParseError
                 |> Debug.log "decodeJSONRPCResult Finish!"
-        ErrorResponse -32700 str -> Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerParseError str)
-        ErrorResponse -32600 str -> Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerInvalidRequest str)
-        ErrorResponse -32601 str -> Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerMethodNotFound str)
-        ErrorResponse -32602 str -> Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerInvalidParams str)
-        ErrorResponse -32603 str -> Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerInternalError str)
-        ErrorResponse code str -> Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerError code str)
+
+        ErrorResponse -32700 str ->
+            Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerParseError str)
+
+        ErrorResponse -32600 str ->
+            Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerInvalidRequest str)
+
+        ErrorResponse -32601 str ->
+            Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerMethodNotFound str)
+
+        ErrorResponse -32602 str ->
+            Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerInvalidParams str)
+
+        ErrorResponse -32603 str ->
+            Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerInternalError str)
+
+        ErrorResponse code str ->
+            Debug.log "decodeJSONRPCResult error case!" <| Result.Err (Web3.Types.ServerError code str)
+
 
 clientVersion : Request String
 clientVersion =
-    request {method = "web3_clientVersionX", params = []} (decodeJSONRPCResult Decode.string)
+    request { method = "web3_clientVersion", params = [] } (decodeJSONRPCResult Decode.string)
+
 
 netVersion : Request String
 netVersion =
-    request {method = "net_version", params = []} (decodeJSONRPCResult Decode.string)
+    request { method = "net_version", params = [] } (decodeJSONRPCResult Decode.string)
+
 
 netListening : Request Bool
 netListening =
-    request {method = "net_listening", params = []} (decodeJSONRPCResult Decode.bool)
+    request { method = "net_listening", params = [] } (decodeJSONRPCResult Decode.bool)
 
-decodeHexInt : Decode.Decoder Int
-decodeHexInt =
-    Decode.map (\x -> 42) Decode.string
+
+
+
 
 netPeerCount : Request Int
-netPeerCount = request {method = "net_peerCount", params = []} (decodeJSONRPCResult decodeHexInt)
+netPeerCount =
+    let
+        responseHandler res =
+            res
+                |> decodeJSONRPCResult Decode.string
+                |> Result.andThen Web3.Utils.hexQuantityToInt
+    in
+        request { method = "net_peerCount", params = [] } responseHandler
--- src/elm/Web3/Utils.elm
+++ src/elm/Web3/Utils.elm
@@ -1,33 +1,14 @@
-module Utils exposing (..)
+module Web3.Utils exposing (..)
+
+import Result
+import Web3.Types exposing (Error)
+import Hex
 
 
-import Regex
-
-type Hex = Hex String
-
-{-| -}
-isHex : String -> Bool
-isHex =
-    Regex.contains (Regex.regex "^((0[Xx]){1})?[0-9a-fA-F]+$")
-
-
-toHex : String -> Hex
-toHex str = 
-
-
-{-| -}
-add0x : String -> String
-add0x str =
-    if String.startsWith "0x" str || String.startsWith "0X" str then
-        str
-    else
-        "0x" ++ str
-
-
-{-| -}
-remove0x : String -> String
-remove0x str =
-    if String.startsWith "0x" str || String.startsWith "0X" str then
-        String.dropLeft 2 str
-    else
-        str
+{-| TODO Bigint? -}
+hexQuantityToInt : String -> Result Error Int
+hexQuantityToInt quantity =
+    quantity
+        |> String.dropLeft 2
+        |> Hex.fromString
+        |> Result.mapError Web3.Types.ResultParseError
