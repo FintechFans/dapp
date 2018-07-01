dbc2894dcc6de4037f38978484918ad49581bee6
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-07-01 23:15:50 +0200

    Potential implementations of map, map2, map3

--- src/elm/Porter/Multi.elm
+++ src/elm/Porter/Multi.elm
@@ -16,7 +16,7 @@ type alias Config req res msg a =
 
 type Msg req res msg a
     = PorterMsg (Porter.Msg req res msg)
-    | ResolveChain (a -> msg) res (Request req res a)
+    | ResolveChain (a -> msg) (Request req res a)
 
 
 type alias Model req res msg =
@@ -42,6 +42,24 @@ andThen reqfun req =
         ComplexRequest porter_req next_request_fun ->
             ComplexRequest porter_req (\res -> andThen reqfun (next_request_fun res))
 
+map : (a -> b) -> Request req res a -> Request req res b
+map mapfun req =
+    case req of
+        SimpleRequest porter_req request_mapper ->
+            SimpleRequest porter_req (request_mapper >> mapfun)
+        ComplexRequest porter_req next_request_fun ->
+            ComplexRequest porter_req (\res -> map mapfun (next_request_fun res))
+
+map2 : (a -> b -> c) -> Request req res a -> Request req res b -> Request req res c
+map2 mapfun req_a req_b =
+    req_a
+        |> andThen (\res_a -> req_b |> map (mapfun res_a))
+
+
+map3 : (a -> b -> c -> d) -> Request req res a -> Request req res b -> Request req res c -> Request req res d
+map3 mapfun req_a req_b req_c =
+    req_a
+        |> andThen (\res_a -> map2 (mapfun res_a) req_b req_c)
 
 send : Config req res msg a -> (a -> msg) -> Request req res a -> Cmd msg
 send config msg_handler request =
@@ -50,7 +68,7 @@ send config msg_handler request =
             Porter.send (config.porter_config) (response_handler >> msg_handler) porter_req
 
         ComplexRequest porter_req next_request_fun ->
-            Porter.send (config.porter_config) (\res -> config.porter_multi_msg (ResolveChain msg_handler res (next_request_fun res))) porter_req
+            Porter.send (config.porter_config) (\res -> config.porter_multi_msg (ResolveChain msg_handler (next_request_fun res))) porter_req
 
 
 update : Config req res msg a -> Msg req res msg a -> Model req res msg -> ( Model req res msg, Cmd msg )
@@ -63,5 +81,9 @@ update config msg model =
             in
                 ( { model | porter_model = porter_model }, porter_cmd )
 
-        ResolveChain msg_handler res request ->
+        ResolveChain msg_handler request ->
             ( model, send config msg_handler request )
+
+subscriptions : Config req res msg a -> Sub msg
+subscriptions config =
+    Porter.subscriptions config.porter_config
