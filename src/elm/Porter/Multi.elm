aad7e27fe5686fad02f27e6f339e4657f91f9bc2
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-07-01 09:53:24 +0200

    cleaning

--- src/elm/Porter/Multi.elm
+++ src/elm/Porter/Multi.elm
@@ -3,11 +3,7 @@ module Porter.Multi exposing (..)
 import Porter
 
 
--- import List.Nonempty exposing (Nonempty(..), (:::))
-
-
 type Request req res a
-    -- = Request (Porter.Request req res) (res -> a) (List ((res -> a), (a -> Request req res a)))
     = SimpleRequest (Porter.Request req res) (res -> a)
     | ComplexRequest (Porter.Request req res) (res -> Request req res a)
 
@@ -37,26 +33,25 @@ request req response_handler =
     SimpleRequest (Porter.request req) response_handler
 
 
--- andThen : ((res -> a), a -> Request req res b) -> Request req res a -> Request req res b
--- andThen (reqfun_a, reqfun_b) (Request porter_req response_handler response_handlers) =
---     Request porter_req reqfun_a ((response_handler, reqfun_b) :: response_handlers)
 andThen : (a -> Request req res b) -> Request req res a -> Request req res b
 andThen reqfun req =
     case req of
         SimpleRequest porter_req request_mapper ->
             ComplexRequest (porter_req) (request_mapper >> reqfun)
+
         ComplexRequest porter_req next_request_fun ->
             ComplexRequest porter_req (\res -> andThen reqfun (next_request_fun res))
-    -- Request req (reqfun >> send config msg_handler)
+
 
 send : Config req res msg a -> (a -> msg) -> Request req res a -> Cmd msg
 send config msg_handler request =
     case request of
         SimpleRequest porter_req response_handler ->
             Porter.send (config.porter_config) (response_handler >> msg_handler) porter_req
+
         ComplexRequest porter_req next_request_fun ->
-            -- Porter.andThen (\a -> send config msg_handler (next_request_fun a)) porter_req
             Porter.send (config.porter_config) (\res -> config.porter_multi_msg (ResolveChain msg_handler res (next_request_fun res))) porter_req
+
 
 update : Config req res msg a -> Msg req res msg a -> Model req res msg -> ( Model req res msg, Cmd msg )
 update config msg model =
@@ -69,4 +64,4 @@ update config msg model =
                 ( { model | porter_model = porter_model }, porter_cmd )
 
         ResolveChain msg_handler res request ->
-            (model, send config msg_handler request)
+            ( model, send config msg_handler request )
