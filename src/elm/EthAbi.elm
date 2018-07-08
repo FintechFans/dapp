3d8bf3796230d5f6cc60a933bb5d8a4afac244e9
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-07-08 10:41:44 +0200

    Type variant documentation

--- src/elm/EthAbi.elm
+++ src/elm/EthAbi.elm
@@ -1,13 +1,11 @@
 module EthAbi exposing (..)
 
-import Hex
 import Char
+import Hex
 
-{-|
 
-TODO using BigInts where required
+{-| TODO using BigInts where required
 TODO testing!
-
 
 Examples:
 
@@ -24,34 +22,56 @@ dynamic_example = EthAbi.encode_args [Static (AbiUint 291), Dynamic (AbiDynamicA
 dynamic_example2 = EthAbi.encode_args [Dynamic (AbiDynamicArray [Dynamic (AbiDynamicArray [Static (AbiUint 1), Static (AbiUint 2)]), Dynamic (AbiDynamicArray [Static (AbiUint 3)])]), Dynamic (AbiDynamicArray [Dynamic (AbiString "one"), Dynamic (AbiString "two"), Dynamic (AbiString "three")])] |> format_output
 
 -}
-
-
-
 type AbiType
     = Static AbiStaticType
     | Dynamic AbiDynamicType
 
 
-type AbiStaticType
+type
+    AbiStaticType
+    -- An unsigned (positive) integer:
     = AbiUint Int
+      -- A signed integer:
     | AbiInt Int
+      -- A boolean value:
     | AbiBool Bool
+      --| A bytes array of known length; i.e bytes3, bytes10, etc:
     | AbiStaticBytes Int String
-    | AbiStaticTuple (List AbiStaticType)
+      --| An array of static length; all elements of the same type:
+    | AbiStaticArray Int (List AbiStaticType)
+      --| A tuple where none of the elements is a dynamic type:
+    | AbiStaticTuple Int (List AbiStaticType)
 
 
-type AbiDynamicType
+type
+    AbiDynamicType
+    -- A bytes array of dynamic length:
     = AbiBytes String
+      -- A string of dynamic length:
     | AbiString String
+      -- An array with dynamic length; all elements of the same type:
     | AbiDynamicArray (List AbiType)
+      -- An array with static length; all elements of the same type:
     | AbiArray Int (List AbiDynamicType)
-    | AbiDynamicTuple (List AbiType) -- ?
+      -- An tuple where one of the elements has to be Dynamic:
+    | AbiDynamicTuple Int (List AbiType)
+
+
+uint : Int -> Result String AbiStaticType
+uint int =
+    if int < 0 then
+        Err "Negative integer cannot be converted to Unsigned Integer"
+    else
+        Ok (AbiUint int)
+
 
 
 {-
- 32 bytes == 64 hexadecimal characters
- This means that when counting hexchars, we get twice the number as when counting bytes.
- -}
+   32 bytes == 64 hexadecimal characters
+   This means that when counting hexchars, we get twice the number as when counting bytes.
+-}
+
+
 encode_args : List AbiType -> String
 encode_args args =
     let
@@ -82,8 +102,11 @@ encode_args args =
             |> List.foldl encode_elem ( "", "" )
             |> concat_tuple
 
+
 bytesInHexString : String -> Int
-bytesInHexString str = String.length str // 2
+bytesInHexString str =
+    String.length str // 2
+
 
 dynamic_tail : AbiDynamicType -> String
 dynamic_tail val =
@@ -104,7 +127,10 @@ dynamic_tail val =
             let
                 len =
                     String.length bytes
-                lhs = static_encode (AbiUint len)
+
+                lhs =
+                    static_encode (AbiUint len)
+
                 rhs =
                     bytes
                         |> strToBytes
@@ -117,15 +143,20 @@ dynamic_tail val =
                 |> AbiBytes
                 |> dynamic_tail
 
-        AbiDynamicTuple vals ->
+        AbiDynamicTuple _ vals ->
             vals
                 |> encode_args
 
+
 padLeftTo32Bytes : Char -> String -> String
-padLeftTo32Bytes char str = String.padLeft 64 char str
+padLeftTo32Bytes char str =
+    String.padLeft 64 char str
+
 
 padRightTo32Bytes : Char -> String -> String
-padRightTo32Bytes char str = String.padRight 64 char str
+padRightTo32Bytes char str =
+    String.padRight 64 char str
+
 
 strToBytes : String -> String
 strToBytes str =
@@ -166,8 +197,10 @@ static_encode val =
             bytes
                 |> strToBytes
                 |> padRightTo32Bytes '0'
-
-        AbiStaticTuple vals ->
+        AbiStaticArray len vals ->
+            static_encode (AbiStaticTuple len vals)
+ 
+        AbiStaticTuple _ vals ->
             vals
                 |> List.map static_encode
                 |> String.concat
